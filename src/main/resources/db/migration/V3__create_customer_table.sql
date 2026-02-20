abbdc1f6af46cd562034d0028a9ae56d1ae640ab
sushil patil <patilsushil794@gmail.com>
2026-02-20 11:30:23 +0530

    rebasing with main

--- src/main/resources/db/migration/V3__create_customer_table.sql
+++ src/main/resources/db/migration/V3__create_customer_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS customer (
-                                        id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     phone VARCHAR(50) NOT NULL UNIQUE,
